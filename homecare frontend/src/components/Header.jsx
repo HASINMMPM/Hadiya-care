c9b0a82204fb9c08ad693f43af0e506e3a85ffb5
muhammad haseen <hasinmmpm786@gmail.com>
2025-02-26 11:04:52 +0530

    added nav

--- homecare frontend/src/components/Header.jsx
+++ homecare frontend/src/components/Header.jsx
@@ -15,65 +15,19 @@ export default function Header() {
             </span>
           </div>
         </a>
-        <div className="flex justify-between w-full lg:w-auto items-center lg:order-2 mt-3 lg:mt-0">
-          <a
-            href="tel://+918139055749"
-            className="text-bg hover:bg-dblue bg-blue focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none duration-300"
-          >
-            Make a Call
-          </a>
-          <button
-            data-collapse-toggle="mobile-menu-2"
-            type="button"
-            className="inline-flex items-center p-2 ml-1 text-sm text-blue rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            aria-controls="mobile-menu-2"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <div
-          className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-          id="mobile-menu-2"
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="lg:hidden p-2 text-blue rounded-lg focus:outline-none"
         >
-          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-dblue hover:text-blue duration-200">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-dblue hover:text-blue duration-200">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-dblue hover:text-blue duration-200">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-dblue hover:text-blue duration-200">
-                Testimonial
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-dblue hover:text-blue duration-200">
-                Contact Us
-              </a>
-            </li>
+          {isOpen ? "✖" : "☰"}
+        </button>
+        <div className={`${isOpen ? "block" : "hidden"} lg:flex w-full lg:w-auto`}>
+          <ul className="flex flex-col lg:flex-row lg:space-x-8">
+            <li><a href="#" className="text-dblue hover:text-blue">Home</a></li>
+            <li><a href="#" className="text-dblue hover:text-blue">About</a></li>
+            <li><a href="#" className="text-dblue hover:text-blue">Services</a></li>
+            <li><a href="#" className="text-dblue hover:text-blue">Testimonial</a></li>
+            <li><a href="#" className="text-dblue hover:text-blue">Contact Us</a></li>
           </ul>
         </div>
       </div>
