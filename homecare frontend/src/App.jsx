1752314e64b79013ce5d5f83bbc7f31b5ec89ff7
muhammad haseen <hasinmmpm786@gmail.com>
2025-02-26 09:53:17 +0530

    initial

---
+++ homecare frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+
+const App = () => {
+  return (
+    <main className="">
+      <Header />
+    </main>
+  );
+};
+
+export default App;
